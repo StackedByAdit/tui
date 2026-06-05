e629eaa407c3b038a3c954c2ca91dfe1d7b3e9e7
Aditya <adityaguptavns1@gmail.com>
2026-06-05 19:12:48 +0530

    complete logout

--- commands/providers/logout.ts
+++ commands/providers/logout.ts
@@ -1,10 +1,18 @@
-
 import { Command } from 'commander';
+import fs from "fs";
 
 export const logoutCommand = new Command("logout")
     .description('Lets user logout from the provider')
     .option('-p, --provider <providerName>', 'Name of the provider (gemini, claude etc)', '')
-    .action((options) => {
-        console.log("logging out for provider " + options.providerName)
+    .action(() => {
+        const configPath = "./providers.json";
+
+        if (!fs.existsSync(configPath)) {
+            console.log("Not logged in to any provider");
+            return;
+        }
+
+        fs.writeFileSync(configPath, JSON.stringify({}, null, 2));
+        console.log("Logged out successfully");
     })
 

2680c0fb856620f5db1ffd3b9346d9b09f6251f3
Aditya <adityaguptavns1@gmail.com>
2026-06-05 10:40:35 +0530

    add login logic

--- commands/providers/login.ts
+++ commands/providers/login.ts
@@ -1,13 +1,32 @@
 import { Command } from 'commander';
 import { homedir } from 'os';
 import path from "path";
+import fs from "fs";
+
+interface option {
+    provider: string;
+    apiKey: string;
+}
 
 export const loginCommand = new Command("login")
     .description('Lets user login into the provider (use it as default)')
     .option('-p, --provider <providerName>', 'Name of the provider (gemini, claude etc)', '')
-    .option('-a, --api_key <apiKey>', 'Your api key', '')
-    .action((options) => {
-        console.log("logging into " + options.provider)
-         const configPath = path.join(homedir())
-         console.log(configPath);
+    .option('-a, --api-key <apiKey>', 'Your api key', '')
+    .action((options: option) => {
+
+        const configPath = "./providers.json";
+
+        let config: Record<string, string> = {};
+        if (fs.existsSync(configPath)) {
+
+            config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+        }
+        
+        config = {
+            provider: options.provider,
+            api_key: options.apiKey
+        };
+
+        fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
+        console.log(`✅ Logged in as ${options.provider}`);
     })

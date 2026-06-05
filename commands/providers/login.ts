3385643afbfa149a05876573623d24ae82208f11
Aditya <adityaguptavns1@gmail.com>
2026-06-05 06:06:55 +0530

    setup

--- commands/providers/login.ts
+++ commands/providers/login.ts
@@ -1,10 +1,13 @@
-
 import { Command } from 'commander';
+import { homedir } from 'os';
+import path from "path";
 
 export const loginCommand = new Command("login")
     .description('Lets user login into the provider (use it as default)')
     .option('-p, --provider <providerName>', 'Name of the provider (gemini, claude etc)', '')
     .option('-a, --api_key <apiKey>', 'Your api key', '')
     .action((options) => {
-        console.log("logging into " + options.providerName)
+        console.log("logging into " + options.provider)
+         const configPath = path.join(homedir())
+         console.log(configPath);
     })

5f5e471298eb13f3a765620df9b8c1681422d829
Aditya <adityaguptavns1@gmail.com>
2026-06-05 19:14:43 +0530

    add models

--- commands/models.ts
+++ commands/models.ts
@@ -1,10 +1,18 @@
 import { Command } from "commander";
+import fs from "fs";
 
 export const modelsCommand = new Command("models")
   .description('Returns all the supported models')
   .option('-m, --model <modelName>', 'name of the model', 'all')
-  .action((options) => {
-    console.log("Listing models...");
+  .action(() => {
+    const modelPath = "./model.json";
 
-    console.log(options)
-});
+    if (!fs.existsSync(modelPath)) {
+      console.log("model.json not found");
+      return;
+    }
+
+    const models = JSON.parse(fs.readFileSync(modelPath, 'utf-8'));
+    console.log("Available models:");
+    console.log(models);
+  })
